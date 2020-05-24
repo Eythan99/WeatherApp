8a0782ba3c12889d1660cd39e2bb6d714828effe
Eythan99 <eythandahan99@gmail.com>
2020-05-24 04:07:09 +0200

    Bonus - Un peu de plus de design + attente d'autres images pour l'ecran détail

--- app/src/main/java/com/example/weatherapp/presentation/view/DetailActivity.java
+++ app/src/main/java/com/example/weatherapp/presentation/view/DetailActivity.java
@@ -10,31 +10,77 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.weatherapp.R;
 import com.example.weatherapp.presentation.Injection;
 import com.example.weatherapp.presentation.model.Weather;
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity{
 
     private TextView txtDetail;
     private ImageView imageWeather;
+    private ImageView imageLeve;
+    private ImageView imageCouche;
+    private TextView txtLeve;
+    private TextView txtCouche;
+    private TextView txtMin;
+    private TextView txtTemp;
+    private TextView txtMax;
+    private TextView txtPressure;
+    private TextView txtHumidity;
+    private TextView txtVisibility;
+    private TextView txtVent;
+    private ImageView imagePressure;
+    private ImageView imageHumidity;
+    private ImageView imageVisibility;
+    private ImageView imageVent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        txtDetail = findViewById(R.id.textView);
+        txtDetail = findViewById(R.id.titre);
         imageWeather = findViewById(R.id.image);
+        imageLeve = findViewById(R.id.leve);
+        imageCouche = findViewById(R.id.couche);
+        txtLeve = findViewById(R.id.leveH);
+        txtCouche = findViewById(R.id.coucheH);
+        txtMin = findViewById(R.id.min);
+        txtTemp = findViewById(R.id.temp);
+        txtMax = findViewById(R.id.max);
+        txtPressure = findViewById(R.id.pression);
+        txtHumidity = findViewById(R.id.humidity);
+        txtVisibility = findViewById(R.id.visibility);
+        txtVent = findViewById(R.id.vent);
+        imagePressure = findViewById(R.id.imagePression);
+        imageHumidity = findViewById(R.id.imageHumidity);
+        imageVisibility = findViewById(R.id.imageVisibility);
+        imageVent = findViewById(R.id.imageVent);
 
         Intent intent = getIntent();
         String weatherJson = intent.getStringExtra("weatherKey");
         Weather weather =  Injection.getGson().fromJson(weatherJson, Weather.class);
         showDetail(weather);
 
-        imageWeather.setImageIcon();
-
     }
 
     private void showDetail(Weather weather) {
-        txtDetail.setText(weather.getAir_pressure());
+        Picasso.get().load("https://www.metaweather.com/static/img/weather/png/" + weather.getWeather_state_abbr() + ".png").into(imageWeather);
+
+        Picasso.get().load("https://github.com/Eythan99/WeatherApp/blob/master/Image/sunrise.png?raw=true").into(imageLeve);
+        Picasso.get().load("https://github.com/Eythan99/WeatherApp/blob/master/Image/sunset.png?raw=true").into(imageCouche);
+
+        txtDetail.setText(weather.getTitle());
+
+        txtLeve.setText(weather.getSun_rise().substring(weather.getSun_rise().indexOf(":")-2, weather.getSun_rise().indexOf(":")+3).replace(":","h"));
+        txtCouche.setText(weather.getSun_set().substring(weather.getSun_set().indexOf(":")-2, weather.getSun_set().indexOf(":")+3).replace(":","h"));
+
+        txtMin.setText(weather.getMin_temp()+"°C");
+        txtTemp.setText(weather.getThe_temp()+"°C");
+        txtMax.setText(weather.getMax_temp()+"°C");
+        txtPressure.setText(weather.getAir_pressure()+" mbar");
+        txtHumidity.setText(weather.getHumidity()+" %");
+        txtVisibility.setText(weather.getVisibility()+" %");
+        txtVent.setText(weather.getWind_speed()+" mph");
+
     }
 
 }
